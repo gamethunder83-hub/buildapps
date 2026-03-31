067ff3d7a2d08afe79e0b85aec1a74ea1a818fa9
gamethunder83-hub <gamethunder83@gmail.com>
2026-03-31 13:34:00 +0530

    Add Firebase project config

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+window.APP_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyBNX1VemDKmg4uWj-YbBzXxcjiLf2g_ZfY",
+    authDomain: "campus-bus-tracker-87dcc.firebaseapp.com",
+    databaseURL: "https://campus-bus-tracker-87dcc-default-rtdb.firebaseio.com",
+    projectId: "campus-bus-tracker-87dcc",
+    storageBucket: "campus-bus-tracker-87dcc.firebasestorage.app",
+    messagingSenderId: "333988615898",
+    appId: "1:333988615898:web:466f774117f0a66b8137e3"
+  },
+  geofenceRadiusMeters: 20,
+  campusCenter: {
+    lat: 12.9716,
+    lng: 77.5946
+  }
+};
